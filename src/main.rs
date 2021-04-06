6709bc0b20649cc4fce822bca8e59a242eb20416
gnullbyte <gnullbyte@gmail.com>
2021-04-05 21:39:36 -0500

    format, check formatting with rusty-tags and prevent commit

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use lex::*;
 fn main() {
     let mut tok = lex::Token::new();
     dfa_whitespace(&mut tok, &String::from("   1d20 + 4"));
-    
+
     println!("tok.lexeme: \"{}\"", tok.lexeme);
     println!("tok.f: {}", tok.f);
 }
