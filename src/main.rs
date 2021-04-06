d994d972b283a0d0b6d4335c0149818945b1690b
gnullbyte <gnullbyte@gmail.com>
2021-04-06 10:20:36 -0500

    clean up example output

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use lex::*;
 use std::convert::TryInto;
 
 fn main() {
-    let test_str2 = String::from(" ** / +++ -   *");
-    let len: i32 = test_str2.len().try_into().unwrap();
+    let test = String::from(" ** / +++ -   *");
+    let len: i32 = test.len().try_into().unwrap();
     let mut pos = 0;
 
+    println!("\"{}\"", test);
     while pos != len {
-        let tok = nfa(pos, &test_str2);
-        println!("tok.ttype: {}", token_type_to_str(tok.ttype));
+        let tok = nfa(pos, &test);
+        println!("{}", token_type_to_str(tok.ttype));
         pos = tok.f;
     }
 }
