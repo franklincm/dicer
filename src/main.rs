1d09493f7362f898e08e57ace31f64a2d4aecfbc
gnullbyte <gnullbyte@gmail.com>
2021-04-06 21:31:20 -0500

    add fn lex(), returns tokens for given string in order

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
 use dicer::*;
 use lex::*;
-use std::convert::TryInto;
 
 fn main() {
-    let test = String::from(
-        "1d20 + 5 * (2d12) <= 5 + MIN - MAX + min() + 1d20 + 5 * (2d12) <= 5 + MIN - MAX + min()",
-    );
-    let len: i32 = test.len().try_into().unwrap();
-    let mut pos = 0;
+    let test = String::from("1d20 + 5 * (2d12) <= 5 + MIN - MAX + min()");
 
-    eprintln!("\"{}\"", test);
-    while pos != len + 1 {
-        let tok = nfa(pos, &test);
-        if tok.ttype != constants::TOKEN_WS {
-            println!("{}", token_type_to_str(tok.ttype));
-        }
-        pos = tok.f;
+    let tokens: Vec<Token> = lex(&test);
+
+    for tok in &tokens {
+        println!("{}", token_type_to_str(tok.ttype));
     }
 }
