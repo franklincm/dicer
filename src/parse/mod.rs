e630c4a5276c15fb0bf808fc20d73e8fe714a26c
gnullbyte <gnullbyte@gmail.com>
2021-04-07 18:55:55 -0500

    add attr for num values

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,3 +1,4 @@
+use crate::lex::constants;
 use crate::lex::lex;
 use crate::lex::Token;
 use dicer::token_type_to_str;
@@ -6,6 +7,10 @@ pub fn get_tokens(src: &String) {
     let tokens: Vec<Token> = lex(src);
 
     for tok in &tokens {
-        println!("{}", token_type_to_str(tok.ttype));
+        if tok.ttype == constants::TOKEN_NUM {
+            println!("{} = {}", token_type_to_str(tok.ttype), tok.attr);
+        } else {
+            println!("{}", token_type_to_str(tok.ttype));
+        }
     }
 }
