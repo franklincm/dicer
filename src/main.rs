e53fc40f9a79352f033628a45eaf2ca0c37c2665
gnullbyte <gnullbyte@gmail.com>
2021-04-12 10:22:26 -0500

    cleanup test run

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
 mod lex;
 mod parse;
 
-//use dicer::*;
 use lex::*;
 
 fn main() {
-    //let test = String::from("1d20 + 5 * (2d12) <= 5 + MIN - MAX + min()");
     let test = String::from("1d2 * (3+2) + min(2, 3)");
     println!("parse: \"{}\"\n", test);
     let mut token: Token = parse::start(&test);
 
     parse::rdp::parse_expression(&mut token, &test);
-
-    // println!("{}", token_type_to_str(token.ttype));
-
-    // parse::match_t(token.ttype, &mut token, &test).unwrap();
-    // println!("{}", token_type_to_str(token.ttype));
-
-    // parse::match_t(constants::TOKEN_D, &mut token, &test).unwrap();
-    // println!("{}", token_type_to_str(token.ttype));
-
-    // parse::match_t(constants::TOKEN_NUM, &mut token, &test).unwrap();
-    // println!("{}", token_type_to_str(token.ttype));
-
-    // parse::match_t(constants::TOKEN_ADDOP, &mut token, &test).unwrap();
-    // println!("{}", token_type_to_str(token.ttype));
-
-    // parse::match_t(constants::TOKEN_NUM, &mut token, &test).unwrap();
-    // println!("{}", token_type_to_str(token.ttype));
 }
