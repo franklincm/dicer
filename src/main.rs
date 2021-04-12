5667df7c582e923baaf75ef4142ea47592b6363a
gnullbyte <gnullbyte@gmail.com>
2021-04-12 12:43:29 -0500

    add fcount test

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ mod parse;
 use lex::*;
 
 fn main() {
+    // simple expression
     let test = String::from("1d2 * (3+2) + min(2, 3)");
     println!("parse: \"{}\"\n", test);
     let mut token: Token = parse::start(&test);
+    parse::rdp::parse_expression(&mut token, &test);
 
+    // fcount
+    let test = String::from("count(5d6, >1, <=5)");
+    println!("\nparse: \"{}\"\n", test);
+    let mut token: Token = parse::start(&test);
     parse::rdp::parse_expression(&mut token, &test);
 }
